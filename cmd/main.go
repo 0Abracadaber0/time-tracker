8b1dc47df759c33d824945374bd8b45fa04b1108
Yauheni <yauhenbelarus@gmail.com>
2024-07-17 10:55:11 +0300

    Added logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"time-tracker/config"
 	"time-tracker/internal/router"
 )
 
 func main() {
+	log := config.SetupLogger(config.Config("ENV"))
+	log.Info("Starting application")
+
 	app := fiber.New()
+
+	app.Use(func(ctx *fiber.Ctx) error {
+		ctx.Locals("logger", log)
+		return ctx.Next()
+	})
 
 	router.SetupRoutes(app)
 
 	err := app.Listen("0.0.0.0:8080")
 	if err != nil {
-		return
+		log.Error(err.Error())
 	}
 }
--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"log/slog"
 	"strings"
 	"time-tracker/config"
 	"time-tracker/internal/models"
@@ -15,9 +16,11 @@ type PassportData struct {
 }
 
 func CreateUserHandler(c *fiber.Ctx) error {
+	log := c.Locals("logger").(*slog.Logger)
 
 	var data PassportData
 	if err := json.Unmarshal(c.Body(), &data); err != nil {
+		log.Error("Invalid JSON format", "statusCode", 400)
 		return c.Status(400).JSON(fiber.Map{
 			"error": "Invalid JSON format",
 		})
@@ -35,18 +38,21 @@ func CreateUserHandler(c *fiber.Ctx) error {
 	params := fmt.Sprintf("/info?passportNumber=%s&passportSerie=%s", data.PassportNumber, data.PassportSerie)
 	req.SetRequestURI(url + params)
 
-	fmt.Println(req.String())
-
 	if err := a.Parse(); err != nil {
-		panic(err)
+		log.Error("Error parsing request", "url", url, "statusCode", 400)
 	}
+	log.Info("Request has been sent", "url", url+params)
 
 	statusCode, body, errs := a.Bytes()
 	if errs != nil {
-		return c.Status(500).JSON(fiber.Map{"error": errs})
+		log.Error("Error parsing request", "url", url, "statusCode", 500)
+		return c.Status(500).JSON(fiber.Map{
+			"error": errs,
+		})
 	}
 
 	if statusCode != 200 {
+		log.Error("Error parsing request", "url", url, "statusCode", statusCode)
 		return c.Status(statusCode).JSON(fiber.Map{
 			"error": "Invalid JSON format",
 		})
@@ -55,10 +61,12 @@ func CreateUserHandler(c *fiber.Ctx) error {
 	var response models.User
 	err := json.Unmarshal(body, &response)
 	if err != nil {
+		log.Error("Error parsing response", "url", url, "statusCode", 400)
 		return c.Status(400).JSON(fiber.Map{
 			"error": "Invalid JSON format",
 		})
 	}
 
+	log.Info("Response has been received", "url", url+params, "statusCode", statusCode)
 	return c.JSON(response)
 }
